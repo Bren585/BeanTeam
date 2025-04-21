2fc51160eb04b05f319e52135fed0d511e0a3f2f
iroha1030 <konamin1030@gmail.com>
2025-04-21 16:21:53 +0900

    Emitter / Projectile

---
+++ BeanTeam/Assets/Scripts/Emitter.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Emitter : MonoBehaviour
+{
+    [SerializeField] private GameObject prefab;    
+    [SerializeField] private float bulletSpeed = 20f;
+
+    public void Shoot(Vector3 position, Vector3 direction)
+    {
+        Debug.Log("Shoot(): " + position + " Å® " + direction);
+
+        GameObject bullet = Instantiate(prefab, position, Quaternion.LookRotation(direction));
+
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.velocity = direction * bulletSpeed;
+        }
+    }
+}
---
+++ BeanTeam/Assets/Scripts/Projectile.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float lifeTime = 3f;
+    [SerializeField] private int damage = 10;
+
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            var e = collision.gameObject.GetComponent<Enemy>();
+            if (e != null) e.TakeDamage(damage);
+        }
+  
+        Destroy(gameObject);
+    }
+}
